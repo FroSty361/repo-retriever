19857c3a5d814985ddb5f37bb62537c8fe9de100
FroSty361 <192673158+FroSty361@users.noreply.github.com>
2026-08-15 21:15:57 -0700

    feat: change font for file-view

--- app/result/routes.py
+++ app/result/routes.py
@@ -17,8 +17,6 @@ def repo_result():
 
             return redirect(url_for('home.index'))
 
-        print(repo_html)
-
         return render_template("result/result.html", repo_name=repoName, repo=Markup(repo_html))
 
     return redirect(url_for('home.index'))
@@ -29,5 +27,10 @@ def view_file(file_url_encoded):
     file_url = file_url_decoded_bytes.decode('utf-8')
 
     file_data = github_api.get_file_data(file_url)
+
+    if file_data is None or file_data == "":
+        print(f"Failed To Get File Data {file_url} From Github Repository")
+
+        return redirect(url_for('home.index'))
 
     return render_template("result/file-view.html", file_name=file_data["name"], file_path=file_data["path"], file_content=file_data["content"])
